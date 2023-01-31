3744adc359a5eba290d934683b47c440bbb141a5
Yosmarli Parica <101285117+Yosmarli@users.noreply.github.com>
2023-01-31 14:19:39 -0400

    Add files via upload

---
+++ numeros.js
@@ -0,0 +1,22 @@
+const alturaCm = 163 
+console.log(alturaCm)
+
+
+const alturaM = 1.63
+console.log(alturaM)
+
+
+const pesoKg = 65.1
+console.log(pesoKg)
+
+
+const alturaMetro = Math.ceil(alturaM)
+console.log(alturaMetro)
+
+
+const pesoKG = Math.floor(pesoKg)
+console.log(pesoKG)
+
+
+const maxValorJs = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(maxValorJs)
